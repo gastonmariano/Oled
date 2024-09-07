6cfa1df56eda5a9f4a808501540e9f9c12771091
mariano <gastonmariano@gmail.com>
2024-09-08 01:55:15 +0200

    agregando un holamundo
    description del holamundo que agrego

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (void)
+{
+
+
+int contador = 0;
+
+    while (contador < 50)
+    {
+        printf("hola mundo\n");
+        contador = contador +1;
+        
+
+    }       
+
+return(1);
+}
